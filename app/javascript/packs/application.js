fdc6ae49a8e7dfa38e160136ee2fe31ed4c5c859
unknown <Administrator@PC2-08.bijisen.local>
2024-08-20 10:59:55 +0900

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,16 @@
+import "bootstrap"
+import "../stylesheets/application"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
+
